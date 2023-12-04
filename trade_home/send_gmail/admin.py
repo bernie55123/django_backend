8cf15e3e40ff36c23ce16119c4b15917f646d234
bo wei chou <bernie3305730@gmail.com>
2023-12-05 00:26:43 +0800

    hot fix

--- trade_home/send_gmail/admin.py
+++ trade_home/send_gmail/admin.py
@@ -4,11 +4,11 @@ from .models import trade_request,Profile,trust_point
 # Register your models here.
 
 class trade_request_admin(admin.ModelAdmin):
-    list_display = ("id","task_name")
+    list_display = ("id","obj_user","task_name")
 admin.site.register(trade_request,trade_request_admin)
 
 class Profile_admin(admin.ModelAdmin):
-    list_display = ("id","obj_user")
+    list_display = ("id","obj_user","balance","trust_point")
 admin.site.register(Profile,Profile_admin)
 
 class trust_point_admin(admin.ModelAdmin):
